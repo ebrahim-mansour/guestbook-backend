6ba0cb9bb613c230b0d3cf36247828098a23c80a
ebrahimmansour <ebrahimmansour2001@gmail.com>
2020-09-13 12:48:13 +0200

    add message and reply model

---
+++ models/message.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+  msgBody: { type: String, required: true },
+  creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+  owner: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+  reply: {
+    creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+    id: { type: mongoose.Types.ObjectId, required: true, ref: "Reply" },
+  },
+});
+
+module.exports = mongoose.model("Message", messageSchema);
